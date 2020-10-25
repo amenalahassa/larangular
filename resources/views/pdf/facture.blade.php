67dd66053bf1cb12f045b805f556ade8fe4f921f
Amen Alahassa <am.konrad21@gmail.com>
2020-10-25 19:38:47 +0100

    Modification de facture pdf

--- resources/views/pdf/facture.blade.php
+++ resources/views/pdf/facture.blade.php
@@ -15,7 +15,7 @@
     </script>
 
     <!-- Fonts -->
-{{--    <link rel="dns-prefetch" href="//fonts.gstatic.com">--}}
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
 {{--    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">--}}
 
 
