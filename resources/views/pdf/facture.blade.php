fbb0836c2849713ed1111f28c43bb58992fc511c
Amen Alahassa <am.konrad21@gmail.com>
2020-10-25 19:45:51 +0100

    Facture pdf modifie pour heroku

--- resources/views/pdf/facture.blade.php
+++ resources/views/pdf/facture.blade.php
@@ -16,13 +16,6 @@
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
-{{--    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">--}}
-
-
-    <!-- Styles -->
-{{--    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">--}}
-
-{{--    <link rel="stylesheet"  href="{{ asset('css/app.css') }}">--}}
     <style>
         {{ file_get_contents('https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css?integrity=sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh&crossorigin=anonymous') }}
     </style>
