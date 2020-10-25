f687d047da8fdc1da5276a3ce43b262d1cd6689f
Amen Alahassa <am.konrad21@gmail.com>
2020-10-25 19:16:43 +0100

    Modification de facture pdf

--- resources/views/pdf/facture.blade.php
+++ resources/views/pdf/facture.blade.php
@@ -10,7 +10,9 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script  src="{{ asset('js/app.js') }}" defer></script>
+    <script  >
+        {{ file_get_contents('js/app.js') }}
+    </script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -20,8 +22,11 @@
     <!-- Styles -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
-    <link rel="stylesheet"  href="{{ asset('css/app.css') }}">
+{{--    <link rel="stylesheet"  href="{{ asset('css/app.css') }}">--}}
 
+    <style>
+        {{ file_get_contents('css/app.css') }}
+    </style>
     <style>
         #loader {
             transition: all 0.3s ease-in-out;
@@ -82,7 +87,7 @@
                                <tbody>
                                <tr>
                                    <td  >
-                                       <img class="about-ent-logo" alt="" src="data:image/jpg;base64,{{ base64_encode(@file_get_contents(asset('images/larangular.png'))) }}"/>
+                                       <img class="about-ent-logo" alt="" src="data:image/jpg;base64,{{ base64_encode(@file_get_contents(asset( $commande->profile_photo_path !== null ? 'storage/public/'. $commande->profile_photo_path : 'images/larangular.png'))) }}"/>
                                        <h4 class="about-ent-name" >{{ $commande->user->name}}</h4>
                                        <h6 > IFU {{ $commande->user->ifu}}</h6>
                                    </td>
